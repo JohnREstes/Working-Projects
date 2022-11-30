aaa09eaef1fa46958f156d08da6537013a686007
John R. Estes <John.R.Estes@icloud.com>
2022-11-29 19:01:39 -0500

    6-Slider
    Finished project, easy

--- Practice_JS/6-Slider/script.js
+++ Practice_JS/6-Slider/script.js
@@ -1,12 +1,26 @@
 const next = document.querySelector('.next');
 const previous = document.querySelector('.previous');
-const backgroundIMGs = ['']
+const slider = document.querySelector('.slider');
+const backgroundIMGs = ['IMG_2630.jpg', 'IMG_2631.jpg','IMG_2632.jpg','IMG_2638.jpg','IMG_2639.jpg', 'IMG_2640.jpg']
+var position = 0;
 
 next.addEventListener('click', ()=>{
-
+  imageSlide("nextSld");
 });
 
 previous.addEventListener('click', ()=>{
-
+  imageSlide("previousSld");
 });
 
+function imageSlide(direction){
+  if (direction == "nextSld" && position < 5){
+    position += 1;
+  } else if (direction == "nextSld" && position == 5){
+    position = 0;
+  } else if (direction == "previousSld" && position > 0){
+    position -= 1
+  } else {
+    position = 5;
+  }
+  slider.style.backgroundImage = "url('" + backgroundIMGs[position] + "')";
+}
