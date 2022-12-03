2417e2eb0f392c37ededb8817fc9943ae7a010a0
John R. Estes <John.R.Estes@icloud.com>
2022-12-02 19:03:26 -0500

    8-Filter
    All buttons operational. Search not coded.

--- Practice_JS/8-Search_and_sort/script.js
+++ Practice_JS/8-Search_and_sort/script.js
@@ -1,14 +1,22 @@
 const buttons = document.querySelectorAll('.btn');
 const items = document.querySelectorAll('.item');
-const classes = ['all', 'cakes', 'cupcakes', 'sweets', 'doughnuts']
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-      console.log(e.target.classList[1]);
         btnClick(e.target.classList[1]);
     })
   })
 
 function btnClick(target){
-    console.log(e.target.classList);
+    items.forEach(item => {
+        item.classList.remove("hidden");
+    })
+    if (target === "all") return;
+    var finalList = [];
+    items.forEach(item => {
+        if(item.classList[1] !== target)finalList.push(item);
+    });
+    finalList.forEach(item => {
+        item.classList.add("hidden");
+    });
 }
