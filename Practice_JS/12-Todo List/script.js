e6d0e0f5646c282a0cfb800e1d9ed86a15737188
John R. Estes <John.R.Estes@icloud.com>
2022-12-10 19:17:20 -0500

    12-todo
    setup, not much progress

---
+++ Practice_JS/12-Todo List/script.js
@@ -0,0 +1,13 @@
+
+var todoListString = `            
+    <div class="item index1">
+    <p class="indvidualItem">What needs Done
+        <span class="right">
+            <span class="material-symbols-rounded green">offline_pin</span>
+            <span class="material-symbols-rounded blue">edit_note</span>
+            <span class="material-symbols-rounded red" >cancel</span>
+        </span>
+    </p>
+    </div>
+`
+const todoItems = document.querySelector('.todoItems');
