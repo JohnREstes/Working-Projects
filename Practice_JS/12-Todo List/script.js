1b2f2f3789b66b09bcea31d672e1f0b6bfd128f6
John R. Estes <John.R.Estes@icloud.com>
2022-12-11 10:56:37 -0500

    12-todo
    setup of basic function, need to figure out edit/aprove and delete

--- Practice_JS/12-Todo List/script.js
+++ Practice_JS/12-Todo List/script.js
@@ -1,13 +1,47 @@
+const todoItems = document.querySelector('.todoItems');
+const inputBox = document.querySelector('.inputBox');
+console.log(todoItems.children);
 
-var todoListString = `            
-    <div class="item index1">
-    <p class="indvidualItem">What needs Done
+inputBox.children[1].onclick = ()=>{
+    let inputText = inputBox.children[0].value;
+    if (inputText === "") alert("Please enter a To Do List Item");
+    else build(inputText);
+}
+
+function randomID(){
+    return Date.now();
+}
+function buttonOp(e){
+    let operation = e.dataset.name;
+    let id = e.id;
+    switch (operation) {
+        case "edit":
+            break;
+        case "delete":
+            break;
+        case "approve":
+            break;
+    }
+
+    console.log(e.dataset.name + "   " + id);
+}
+function newItems(){
+    build();
+}
+function build(text){
+    let randID = randomID();
+    let newItemDiv = document.createElement("div");
+    newItemDiv.className = `item ${randID}`;
+    var todoListString = `            
+    <p class="indvidualItem">${text}
         <span class="right">
-            <span class="material-symbols-rounded green">offline_pin</span>
-            <span class="material-symbols-rounded blue">edit_note</span>
-            <span class="material-symbols-rounded red" >cancel</span>
+            <span onclick=buttonOp(this) data-name="approve" class="material-symbols-rounded inactive" id="${randID}">offline_pin</span>
+            <span onclick=buttonOp(this) data-name="edit" class="material-symbols-rounded blue" id="${randID}">edit_note</span>
+            <span onclick=buttonOp(this) data-name="delete" class="material-symbols-rounded red" id="${randID}">cancel</span>
         </span>
     </p>
-    </div>
-`
-const todoItems = document.querySelector('.todoItems');
+    `
+    todoItems.appendChild(newItemDiv);
+    todoItems.lastChild.innerHTML = todoListString
+    return 
+}
