1486c3e45d1d317d20751c01157afd5835c4ad96
John R. Estes <John.R.Estes@icloud.com>
2022-12-15 21:10:04 -0500

    personal site
    sticky works, still got a jumpy spot that needs corrected.

--- scrollMagic.js
+++ scrollMagic.js
@@ -5,14 +5,6 @@ const first = document.querySelector('.first')
 const navbar = document.getElementById("navbar");
 const stickyNav = navbar.offsetTop;
 
-console.log(window.innerHeight);
-console.log(stickyNav);
-
-onresize = (event) => {
-    console.log(window.innerHeight);
-    console.log(sticky);   
-};
-
 const observer = new IntersectionObserver(entries => {
     console.log(entries);
     navbar.classList.toggle("sticky", !(entries[0].isIntersecting));
