71209628e75f1404ac39e68cf5e75a7b5ed9a1f4
John R. Estes <John.R.Estes@icloud.com>
2022-12-15 19:18:11 -0500

    Personal Website
    Working with changing position of nav bar to sticky.

--- scrollMagic.js
+++ scrollMagic.js
@@ -1,25 +1,23 @@
+const root = document.querySelector(':root')
 const body = document.querySelector('body');
 const container = document.querySelector('.container');
+const first = document.querySelector('.first')
 const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
+const stickyNav = navbar.offsetTop;
 
-console.log(sticky);
+console.log(window.innerHeight);
+console.log(stickyNav);
 
-/*container.onscroll = (e)=>{
-    myFunction();
-}
+onresize = (event) => {
+    console.log(window.innerHeight);
+    console.log(sticky);   
+};
 
-function myFunction() {
-    console.log("run");
-    console.log(window.pageYOffset);
-  if (window.pageYOffset >= sticky) {
-  navbar.classList.add("sticky")
-  } else {
-  navbar.classList.remove("sticky");
-  }
-}*/
-
-container.addEventListener("scroll", ()=>{
-    console.log(window.pageXOffset);
-    console.log(window.pageYOffset);   
+const observer = new IntersectionObserver(entries => {
+    console.log(entries);
+    navbar.classList.toggle("sticky", !(entries[0].isIntersecting));
+},{
+    rootMargin: (stickyNav - window.innerHeight + "px")
 });
+
+observer.observe(first)
