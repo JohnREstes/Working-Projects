9f6773e6ac33551194b617a39e05b42e644f12d9
John R. Estes <John.R.Estes@icloud.com>
2023-01-05 22:20:18 -0500

    calc
    Changes to styles and building class in JS

--- Practice_JS/10-Calculator Revised/script.js
+++ Practice_JS/10-Calculator Revised/script.js
@@ -1,3 +1,5 @@
+class
+
 const numberButtons = document.querySelectorAll([data-number]);
 const operationButton = document.querySelectorAll([data-operation]);
 const equalsButton = document.querySelector([data-equals]);
@@ -5,3 +7,5 @@ const deleteButton = document.querySelector([data-deletes]);
 const allClearButton = document.querySelector([data-all-clear]);
 const previousOperand = document.querySelector([data-previous-operand]);
 const currentOperand = document.querySelector([data-current-operand]);
+
+
