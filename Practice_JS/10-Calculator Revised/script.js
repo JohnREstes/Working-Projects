129e24a53f825dc3a981d3ed9f9ccf2aba13be8d
John R. Estes <John.R.Estes@icloud.com>
2023-01-06 19:44:54 -0500

    calc
    class setup

--- Practice_JS/10-Calculator Revised/script.js
+++ Practice_JS/10-Calculator Revised/script.js
@@ -1,11 +1,37 @@
-class
+class Calculator {
+    constructor(previousOperandText, currentOperandText){
+        this.previousOperandText = previousOperandText
+        this.currentOperandText = currentOperandText
+        this.clear()
+    }
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+
+    }
+    chooseOperation(operation){
+
+    }
+    compute(){
+
+    }
+    updateDisplay(){
+
+    }
+}
 
 const numberButtons = document.querySelectorAll([data-number]);
 const operationButton = document.querySelectorAll([data-operation]);
 const equalsButton = document.querySelector([data-equals]);
 const deleteButton = document.querySelector([data-deletes]);
 const allClearButton = document.querySelector([data-all-clear]);
-const previousOperand = document.querySelector([data-previous-operand]);
-const currentOperand = document.querySelector([data-current-operand]);
+const previousOperandText = document.querySelector([data-previous-operand-text]);
+const currentOperandText = document.querySelector([data-current-operand-text]);
 
 
