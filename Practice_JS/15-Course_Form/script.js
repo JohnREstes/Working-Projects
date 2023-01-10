912e3e171464c4bd1b8deff9bacedcfe6b83bf19
John R. Estes <John.R.Estes@icloud.com>
2023-01-09 20:16:13 -0500

    course
    initial setup on

---
+++ Practice_JS/15-Course_Form/script.js
@@ -0,0 +1,5 @@
+const cutomer = document.querySelector('.customer');
+const course = document.querySelector('.course');
+const author = document.querySelector('.author');
+const submit = document.querySelector('.submitBtn');
+
