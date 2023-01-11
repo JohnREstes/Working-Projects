8c1953e9d3a11a4a9ebebc282275f6557f57b478
John R. Estes <John.R.Estes@icloud.com>
2023-01-10 19:09:44 -0500

    added node modules
    in error

--- Practice_JS/15-Course_Form/script.js
+++ Practice_JS/15-Course_Form/script.js
@@ -2,4 +2,11 @@ const cutomer = document.querySelector('.customer');
 const course = document.querySelector('.course');
 const author = document.querySelector('.author');
 const submit = document.querySelector('.submitBtn');
+const url = 'https://api.unsplash.com/photos/random?query=person&orientation=landscape&client_id=RazJYuaxnGXQlXDYzgF3rKLAt6d0Xz8XeQ3JC6vive8';
 
+function newImage(){
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json.urls.small))
+    .catch((error) => console.log(error));
+}
