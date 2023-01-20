006a995296fd626a4e47b41bfdff7994f825c791
John R. Estes <John.R.Estes@icloud.com>
2023-01-20 12:15:31 -0500

    flash-card
    local storage

--- Practice_JS/16-Flash_Cards/script.js
+++ Practice_JS/16-Flash_Cards/script.js
@@ -61,14 +61,14 @@ function mutateLocSto(id, item = '', operation){
     let cardList = JSON.parse(window.localStorage.getItem('flashCards'));
     switch(operation){
         case "add":
-            cardList.push(new List_Item(id, item));
+            cardList.push(new FlashCard(id, item));
             break;
         case "remove":
             cardList = grocList.filter(e => e.id !== id);
             break;
         case "edit":
             cardList = grocList.filter(e => e.id !== id);
-            cardList.push(new List_Item(id, item));
+            cardList.push(new FlashCard(id, item));
             break;
         case "retrieve":
             return cardList;
@@ -81,4 +81,28 @@ function mutateLocSto(id, item = '', operation){
     }
     window.localStorage.setItem('flashCards', JSON.stringify(cardList));
     console.log(JSON.parse(window.localStorage.getItem('flashCards')));
+}
+function build(randID, text){
+    if(randID == '') {
+        randID = randomID()
+        mutateLocSto(randID, text, "add");
+    }
+    let newItemDiv = document.createElement("div");
+    newItemDiv.className = 'item';
+    newItemDiv.id = `${randID}item`;
+    var todoListString = `            
+                <span class="indvidualItem">
+                    <p class="itemText" id="${randID}text">${text}</p>
+                    <input type="text" class="inputInactive inputItem" placeholder="">
+                    <span class="right">
+                        <span onclick=buttonOp(this) data-name="approve" class="material-symbols-rounded inactive" id="${randID}approve">offline_pin</span>
+                        <span onclick=buttonOp(this) data-name="edit" class="material-symbols-rounded blue" id="${randID}edit">edit_note</span>
+                        <span onclick=buttonOp(this) data-name="delete" class="material-symbols-rounded red" id="${randID}delete">cancel</span>
+                    </span>
+                </span>
+            </div>
+    `
+    todoItems.appendChild(newItemDiv);
+    todoItems.lastChild.innerHTML = todoListString
+    return 
 }
