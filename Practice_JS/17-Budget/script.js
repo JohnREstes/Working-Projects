7b11dbf10a86cb0cfb8e95d5be09ca7549ec1fab
John R. Estes <John.R.Estes@icloud.com>
2023-02-02 20:39:28 -0500

    budget
    added expense to feild

--- Practice_JS/17-Budget/script.js
+++ Practice_JS/17-Budget/script.js
@@ -4,27 +4,36 @@ const expenseType = document.querySelector('[data-expense-type]');
 const expenseDollar = document.querySelector('[data-expense-dollar]');
 const expButton = document.querySelector('[data-add]');
 const budgetDollarTotal = document.querySelector('[data-budget-dollar-total]');
+const expenseDollarTotal = document.querySelector('[data-expense-dollar-total]');
+const balanceDollarTotal = document.querySelector('[data-balance-dollar-total]');
 const runningTotal = document.querySelector('[data-running-total]');
 
-var budget = 0, expense = 0, balance = 0;
+budgetDollarTotal.textContent = formatUSD(0);
+expenseDollarTotal.textContent = formatUSD(0);
+balanceDollarTotal.textContent = formatUSD(0);
 
 calcButton.onclick = ()=>{
     budgetDollarTotal.textContent = formatUSD(budgetDollar.value);
-    budgetDollar.value = ''
+    budgetDollar.value = '';
+    balanceDollarTotal.textContent = formatUSD((parseFloat((budgetDollarTotal.textContent).replace(/[^0-9\.-]+/g,""))) - (parseFloat((expenseDollarTotal.textContent).replace(/[^0-9\.-]+/g,""))));
 };
 expButton.onclick = ()=>{
     expenseType.value === '' ? expenseType.classList.add('missing') : expenseType.classList.remove('missing');
     expenseDollar.value === '' ? expenseDollar.classList.add('missing') : expenseDollar.classList.remove('missing');
     if(expenseType.value === '' || expenseDollar.value === "") return;
+    expenseDollarTotal.textContent = formatUSD(expenseDollar.value);
+    expenseDollar.value = '';  
+    balanceDollarTotal.textContent = formatUSD((parseFloat((budgetDollarTotal.textContent).replace(/[^0-9\.-]+/g,""))) - (parseFloat((expenseDollarTotal.textContent).replace(/[^0-9\.-]+/g,""))));
     let type = expenseType.value;
     let dollar = expenseDollar.value;
-    buildEntry(type, dollar);
+    //buildEntry(type, dollar);
 }
 
 function formatUSD(amount){
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' } ).format(parseFloat(amount));
 }
 
-function buildEntry(type, dollar){
 
+function buildEntry(type, dollar){
+    let newLine = 14;
 }
