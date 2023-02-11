1e8ca97e1c4cf870a7e4254fe2dceba72491a675
John R. Estes <John.R.Estes@icloud.com>
2023-02-11 18:11:41 -0500

    random_User
    Setup

---
+++ Practice_JS/21-Random_User/script.js
@@ -0,0 +1,39 @@
+const changeButton = document.querySelector('.changeButton');
+const randomJoke = document.querySelector('[data-joke]');
+const profilePicture = document.querySelector('[data-profile-picture]')
+
+profilePicture.style.backgroundImage = "url('John_Headshot.jpg')";
+
+changeButton.addEventListener("click", ()=>{
+  newJoke();
+ });
+
+function newJokes(){
+  fetch('https://api.chucknorris.io/jokes/random', {
+  method: 'GET'
+})
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result.value);
+     return result.value;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+}
+
+async function newJoke() {
+  const response = await fetch('https://api.chucknorris.io/jokes/random', {
+    method: 'GET'
+  });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  const joke = await response.json();
+  randomJoke.textContent = joke.value;
+}
+newJoke().catch(error => {
+  error.message; // 'An error has occurred: 404'
+});
