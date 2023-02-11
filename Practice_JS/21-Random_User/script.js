55b51ead9dfc3879ed0d129d244a243d6b9b687c
John R. Estes <John.R.Estes@icloud.com>
2023-02-11 18:41:50 -0500

    random_user
    setup async get request and Object builder

--- Practice_JS/21-Random_User/script.js
+++ Practice_JS/21-Random_User/script.js
@@ -1,39 +1,38 @@
-const changeButton = document.querySelector('.changeButton');
-const randomJoke = document.querySelector('[data-joke]');
-const profilePicture = document.querySelector('[data-profile-picture]')
-
-profilePicture.style.backgroundImage = "url('John_Headshot.jpg')";
-
-changeButton.addEventListener("click", ()=>{
-  newJoke();
- });
-
-function newJokes(){
-  fetch('https://api.chucknorris.io/jokes/random', {
-  method: 'GET'
-})
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result.value);
-     return result.value;
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-
+class User {
+  constructor(name, email, dob, location, cell, password){
+    this.name = name,
+    this.email = email,
+    this.dob = dob,
+    this.location = location,
+    this.cell = cell,
+    this.password = password;
+  }
 }
 
-async function newJoke() {
-  const response = await fetch('https://api.chucknorris.io/jokes/random', {
+const changeButton = document.querySelector('.changeButton');
+const profile = document.querySelectorAll('[data-profile]');
+const icon = document.querySelectorAll('[data-icon]')
+let currentUser;
+
+profile[0].style.backgroundImage = "url('John_Headshot.jpg')";
+
+changeButton.addEventListener("click", ()=>{
+  newUser();
+ });
+
+async function newUser() {
+  const response = await fetch('https://randomuser.me/api/', {
     method: 'GET'
   });
   if (!response.ok) {
     const message = `An error has occured: ${response.status}`;
     throw new Error(message);
   }
-  const joke = await response.json();
-  randomJoke.textContent = joke.value;
+  const user = await response.json();
+  console.log(user.results[0]);
+  currentUser = new User(user.results[0].name, user.results[0].email, user.results[0].dob, user.results[0].location, user.results[0].cell, user.results[0].login.password);
+  console.log(currentUser);
 }
-newJoke().catch(error => {
+newUser().catch(error => {
   error.message; // 'An error has occurred: 404'
 });
