1ddc7c4e093ae0d2615ab0fe4bb77aadc65ee441
John R. Estes <John.R.Estes@icloud.com>
2023-02-15 12:48:17 -0500

    hangman
    words on screen, keyboard works

--- Practice_JS/21-Hangman/script.js
+++ Practice_JS/21-Hangman/script.js
@@ -1,1 +1,47 @@
+class Answer {
+  constructor(answer){
+    this.answer = answer,
+    this.answerLength = answer.length
+  }
+  buildAnswer(){
+    let splitAnswer = this.splitOnSpace();
+    console.log(splitAnswer[0]);
+    for(let j = 0; j < splitAnswer.length; j++){
+      for(let i = 0; i < splitAnswer[j].length; i++){
+        let tempElement = document.createElement('div');
+        tempElement.classList.add('letter');
+        let indLetter = splitAnswer[j].split('');
+        tempElement.textContent = indLetter[i].toUpperCase();
+        answerGrid.appendChild(tempElement);
+      }
+      let tempElement = document.createElement('div');
+      tempElement.classList.add('letter');
+      tempElement.classList.add('space');
+      answerGrid.appendChild(tempElement); 
+    }
+
+  }
+  splitOnSpace(){
+    let answerArray = [];
+    return answerArray = this.answer.split(' ');
+  }
+}
+
 const answerList = ['Eiffle Tower', 'The Colosseum', 'Statue of Liberty', 'Machu Picchu', 'The Acropolis', 'The Taj Mahal', 'Pyramids of Giza', 'Great Wall of China'];
+const hangman = document.querySelectorAll('.man');
+const answerGrid = document.querySelector('.answerGrid');
+const dataKey = document.querySelectorAll('.key');
+
+buildBoard();
+
+dataKey.forEach(key =>{
+  key.addEventListener('click', (e)=>{
+    console.log(e.target.dataset.key);
+  })
+})
+
+function buildBoard(){
+  let randomAnswer = Math.floor((Math.random() * answerList.length))
+  let newAnswer = new Answer(answerList[randomAnswer]);
+  newAnswer.buildAnswer();
+}
