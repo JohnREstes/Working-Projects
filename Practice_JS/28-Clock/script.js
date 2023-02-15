158eefa44846fc6c416166dea1bca9b3c735f05d
John R. Estes <John.R.Estes@icloud.com>
2023-02-14 21:40:16 -0500

    clock
    easy project

--- Practice_JS/28-Clock/script.js
+++ Practice_JS/28-Clock/script.js
@@ -1,12 +1,33 @@
-const changeButton = document.querySelector('.changeButton');
 const root = document.documentElement;
-const backgroundColor = ['--rgb1', '--rgb2', '--rgb3'];
+const secondDeg = '--secondDeg';
+const minuteDeg = '--minuteDeg';
+const hourDeg = '--hourDeg';
+let milli = 0;
+let second = 0;
+let minute = 0;
+let hour = 0;
 
-changeButton.addEventListener("click", ()=>{
-  backgroundColor.forEach(randomColorRBG);
- });
-
-function randomColorRBG(rgb){
-    var colorValue = Math.floor(Math.random() * (255 + 1));
-    root.style.setProperty(rgb, colorValue);
+function updateTime(){
+  let newDate = new Date();
+  milli = newDate.getMilliseconds();
+  console.log(milli);
+  second = newDate.getSeconds();
+  minute = newDate.getMinutes();
+  hour = newDate.getHours();
 }
+
+function setTime(){
+  if(hour > 12) hour -= 12;
+  root.style.setProperty(secondDeg, ((second + (milli / 1000)) * 6));
+  root.style.setProperty(minuteDeg, ((minute + (second / 60)) * 6));  
+  root.style.setProperty(hourDeg, ((hour + (minute / 60)) * 30));
+  console.log(`${hour}:${minute}:${second}`);
+}
+
+updateTime();
+setTime();
+
+setInterval(()=>{
+  setTime();
+  updateTime()
+}, 100);
