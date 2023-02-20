cc309af04d2726c6395727132269f016fde2e5d8
John R. Estes <John.R.Estes@icloud.com>
2023-02-20 15:19:42 -0500

    firebase
    basics set.  Need to implement

--- Practice_JS/23-Firebase_Multi/script.js
+++ Practice_JS/23-Firebase_Multi/script.js
@@ -1,21 +1,82 @@
 const changeButton = document.querySelector(".changeButton");
 const root = document.documentElement;
 const backgroundColor = ["--rgb1", "--rgb2", "--rgb3"];
+let color = [];
 
 changeButton.addEventListener("click", () => {
+  color = [];
   backgroundColor.forEach(randomColorRBG);
+  handleChange()
 });
 
 function randomColorRBG(rgb) {
   var colorValue = Math.floor(Math.random() * (255 + 1));
   root.style.setProperty(rgb, colorValue);
+  color.push(colorValue);
 }
 
-  console.log("HI")
+function handleChange(){
+  //update players[playerId].value = ?
+  //then set change
+  players[playerId].name = "John";
+  playerRef.set(players[playerId]);
+}
+
+function initGame(){
+  //get all players
+  const allPlayersRef = firebase.database().ref('players');
+
+  //callback when allPlayersRef changes
+  allPlayersRef.on('value', (snapshot) => {
+    //set any player value change to player object
+    players = snapshot.val() || {};
+    //loop through player object and set dom elements
+    Object.keys(players).forEach((key)=>{
+      const characterState = players[key];
+      let el = playerElements[key];
+      //update Dom
+      //el.queryselector('.name/move') = characterState.name/move
+    })
+
+  })
+  //callback when new child compared to what your browser knows
+  allPlayersRef.on('child_added', (snapshot) => {
+    const addedPlayer = snapshot.val();
+    const characterElement = document.createElement('div');
+    if(addedPlayer.id === playerId.id){
+      //this is you
+    }
+    //render player to screen
+    playerElements[addedPlayer.id] = characterElement;
+  })
+
+
+}
+
+  let playerId, playerRef;
+  let players = {};
+  let playerElements = {};
+
   firebase.auth().onAuthStateChanged((user) => {
     console.log(user);
     if (user) {
-      //You are logged in
+      playerId = user.uid;
+      playerRef = firebase.database().ref(`players/${playerId}`);
+
+      const name = createName();
+
+      playerRef.set({
+        id: playerId,
+        name,
+        color: 'default'
+      });
+
+      //removes player on browser close
+      playerRef.onDisconnect().remove();
+
+      //begin game
+      initGame();
+
     } else {
       //you are not logged in
     }
@@ -31,3 +92,15 @@ function randomColorRBG(rgb) {
       console.log(errorCode, errorMessage);
     });
 
+function randomFromArray(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+function createName(){
+  const prefix = randomFromArray([
+    'COOL', 'SUPER', 'HIP', 'SMUG', 'SILKY', 'GOOD', 'RAD', 'DOPE', 'BUFF'
+  ])
+  const animal = randomFromArray([
+    'DOG', 'CAT', 'BEAR', 'LAMB', 'LION', 'FOX', 'RAT', 'GOAT', 'BIRD'
+  ])  
+  return `${prefix} ${animal}`
+}
