501210d1b2a361e59936219bdf2794a81ce8560c
John R. Estes <John.R.Estes@icloud.com>
2023-03-07 07:22:02 -0500

    war
    added svg to main directory, setup access to cards

--- Practice_JS/24-War/script.js
+++ Practice_JS/24-War/script.js
@@ -1,12 +1,13 @@
-const changeButton = document.querySelector('.changeButton');
-const root = document.documentElement;
-const backgroundColor = ['--rgb1', '--rgb2', '--rgb3'];
+const deck = ['2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD']
 
-changeButton.addEventListener("click", ()=>{
-  backgroundColor.forEach(randomColorRBG);
- });
+const currentCard = document.getElementById('currentCard');
 
-function randomColorRBG(rgb){
-    var colorValue = Math.floor(Math.random() * (255 + 1));
-    root.style.setProperty(rgb, colorValue);
+let pause = setInterval(changeCard, 250);
+
+let count = 0;
+
+function changeCard(){
+  currentCard.src = `./cards/${deck[count]}.svg`
+  count++
+  if(count === 52) clearInterval(pause);
 }
