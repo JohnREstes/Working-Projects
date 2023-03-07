50436e048699218e3ab7748f037779ce3d7efa8b
John R. Estes <John.R.Estes@icloud.com>
2023-03-07 07:36:24 -0500

    war
    added a fisher-yates shuffle

--- Practice_JS/24-War/script.js
+++ Practice_JS/24-War/script.js
@@ -1,13 +1,26 @@
 const deck = ['2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD']
 
 const currentCard = document.getElementById('currentCard');
+let playerHand, computerHand;
 
-let pause = setInterval(changeCard, 250);
+let pause = setInterval(changeCard, 125);
 
 let count = 0;
 
 function changeCard(){
-  currentCard.src = `./cards/${deck[count]}.svg`
+  let shuffledDeck = shuffle(deck);
+  currentCard.src = `./cards/${shuffledDeck[count]}.svg`
   count++
   if(count === 52) clearInterval(pause);
+}
+function shuffle(array) {
+  const newArray = [...array]
+  const length = newArray.length
+  for (let start = 0; start < length; start++) {
+    const randomPosition = Math.floor((newArray.length - start) * Math.random())
+    const randomItem = newArray.splice(randomPosition, 1)
+
+    newArray.push(...randomItem)
+  }
+  return newArray
 }
