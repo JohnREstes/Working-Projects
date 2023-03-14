4c5862f83e6407ec56d3273d25b5e971cce467a6
John R. Estes <John.R.Estes@icloud.com>
2023-03-14 08:52:00 -0500

    RPSLS
    Setup to remove event listener and to set computer id correctly

--- Practice_JS/25-RPSLS/script.js
+++ Practice_JS/25-RPSLS/script.js
@@ -8,26 +8,32 @@ const WINNING_COMBO = [['scissorsImg', 'paperImg', 'cuts'], ['paperImg', 'rockIm
 const IMG_SRC = ["./img/Rock.svg", "./img/Paper.svg", "./img/Scissors.svg", "./img/Lizard.svg", "./img/Spock.svg"];
 
 options.forEach(option =>{
-    option.addEventListener('click', initGame);
+    option.addEventListener('click', initGame)
 })
 function initGame(e){
-    console.log(e);
-    removeClick();
     computerRandom();
-    checkWin(target.target.id);
+    checkWin(e);
+    removeAllClickEvents();
 }
 function checkWin(target){
-    console.log(target);
+    let computerPlay = computerMoves.children[0].id;
+    let playerPlay = target.target.id;
+
+    console.log(computerPlay, playerPlay);
 }
 function computerRandom(){
     let random = Math.floor(Math.random() * 5);
     let tempImg = document.createElement('img');
-    tempImg.id = "computerChoice" 
+    let tempId = IMG_SRC[random];
+    tempId = tempId.substring(6, tempId.length);
+    tempId = tempId.substring(0, tempId.length - 4);    
+    tempId = tempId.toLowerCase() + "Img";
+    tempImg.id = tempId;
     tempImg.src = IMG_SRC[random];
     computerMoves.appendChild(tempImg);
 }
-function removeClick(){
+function removeAllClickEvents(){
     options.forEach(option =>{
-        option.removeEventListener ('click', initGame);
+        option.removeEventListener('click', initGame);
     })
 }
