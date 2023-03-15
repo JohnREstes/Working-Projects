7cb4dec59362f88aa2c919e19d1c3c2b091aff2c
John R. Estes <John.R.Estes@icloud.com>
2023-03-15 15:36:14 -0500

    RPSLS
    retrieving and storing score to localStorage

--- Practice_JS/25-RPSLS/script.js
+++ Practice_JS/25-RPSLS/script.js
@@ -1,16 +1,21 @@
+const WINNING_COMBO = [['scissorsImg', 'paperImg', 'cuts'], ['paperImg', 'rockImg', 'covers'], ['rockImg', 'lizardImg', 'crushes'], ['lizardImg', 'spockImg', 'poisons'], ['spockImg', 'scissorsImg', 'smashes'], ['scissorsImg', 'lizardImg', 'decapitates'], ['lizardImg', 'paperImg', 'eats'], ['paperImg', 'spockImg', 'disproves'], ['spockImg', 'rockImg', 'vaporizes'], ['rockImg', 'scissorsImg', 'crushes']];
+const IMG_SRC = ["./img/Rock.svg", "./img/Paper.svg", "./img/Scissors.svg", "./img/Lizard.svg", "./img/Spock.svg"];
+
 const options = document.querySelectorAll('.options');
 const computerMoves = document.getElementById('computerMoves');
 const reset = document.getElementById('reset');
 const playerScore = document.getElementById('playerScore');
 const computerScore = document.getElementById('computerScore');
 const winner = document.getElementById('whoWon');
-const WINNING_COMBO = [['scissorsImg', 'paperImg', 'cuts'], ['paperImg', 'rockImg', 'covers'], ['rockImg', 'lizardImg', 'crushes'], ['lizardImg', 'spockImg', 'poisons'], ['spockImg', 'scissorsImg', 'smashes'], ['scissorsImg', 'lizardImg', 'decapitates'], ['lizardImg', 'paperImg', 'eats'], ['paperImg', 'spockImg', 'disproves'], ['spockImg', 'rockImg', 'vaporizes'], ['rockImg', 'scissorsImg', 'crushes']];
-const IMG_SRC = ["./img/Rock.svg", "./img/Paper.svg", "./img/Scissors.svg", "./img/Lizard.svg", "./img/Spock.svg"];
+let score;
+ 
+storeLocal();
 
 options.forEach(option =>{
     option.addEventListener('click', initGame)
 })
 function initGame(e){
+    e.target.classList.add('larger');
     computerRandom();
     checkWin(e);
     removeAllClickEvents();
@@ -20,7 +25,7 @@ function checkWin(target){
     let playerPlay = target.target.id;
     let win = ''
     if(computerPlay === playerPlay){
-        console.log("Tie");
+        winner.innerText = capitalize("tie");
         return;
     }
     for(option of WINNING_COMBO){
@@ -28,15 +33,30 @@ function checkWin(target){
     }
     console.log(computerPlay, playerPlay);
     console.log(win);
-    if(computerPlay === win[0]){
-        console.log(`Computer Wins!  ${win[0].substring(0, win[0].length - 3)} ${win[2]} ${win[1].substring(0, win[1].length - 3)}`);
+
+    if(computerPlay == win[0]){
+        score[1] = 1;
+        winner.innerText = (
+            `Computer Wins!  
+            ${capitalize(win[0].substring(0, win[0].length - 3))} ${win[2]} ${capitalize(win[1].substring(0, win[1].length - 3))}`);
     } else {
-        console.log(`Player Wins!  ${win[0].substring(0, win[0].length - 3)} ${win[2]} ${win[1].substring(0, win[1].length - 3)}`);
+        score[0] = 1;
+        winner.innerText = (
+            `Player Wins!  
+            ${capitalize(win[0].substring(0, win[0].length - 3))} ${win[2]} ${capitalize(win[1].substring(0, win[1].length - 3))}`);
     }
+    console.log(score[0]);
+    console.log(score[1]);
+    updateScore(score);
+}
+function capitalize(word)
+{
+    return word[0].toUpperCase() + word.slice(1);
 }
 function computerRandom(){
     let random = Math.floor(Math.random() * 5);
     let tempImg = document.createElement('img');
+    tempImg.classList.add('larger');
     let tempId = IMG_SRC[random];
     tempId = tempId.substring(6, tempId.length);
     tempId = tempId.substring(0, tempId.length - 4);    
@@ -49,4 +69,14 @@ function removeAllClickEvents(){
     options.forEach(option =>{
         option.removeEventListener('click', initGame);
     })
+}
+function storeLocal(){
+    if(!localStorage.getItem('score')){
+        score = localStorage.setItem('score', JSON.stringify([0, 0]));
+    }
+    score = JSON.parse(localStorage.getItem('score'));
+}
+function updateScore(currentScore){
+    console.log(currentScore[0]);
+    console.log(currentScore[1]);
 }
