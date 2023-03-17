e5b0341c393bc56e13f5cab0cf38d4b35bb0bfad
John R. Estes <John.R.Estes@icloud.com>
2023-03-16 21:33:18 -0500

    weather
    updated HTML data set and typo
    set JS to access dataset

--- Practice_JS/26-Weather_App/script.js
+++ Practice_JS/26-Weather_App/script.js
@@ -1,5 +1,7 @@
 const URL = 'https://api.open-meteo.com/v1/forecast?latitude=20.36&longitude=-87.59&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,weathercode,cloudcover,windspeed_10m&daily=weathercode,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,precipitation_probability_max,windspeed_10m_max&current_weather=true&timezone=America%2FJamaica';
 
+const data = document.querySelectorAll(`[data-current]`);
+const forecast = document.querySelectorAll(`[data-forecast]`);
 let json;
 
 async function pullWeather(){
@@ -17,6 +19,8 @@ async function pullWeather(){
       
       if (json) {
         console.log('Use the JSON here!', json);
+        console.log(data);
+        console.log(forecast);
       }
 }
 pullWeather();
