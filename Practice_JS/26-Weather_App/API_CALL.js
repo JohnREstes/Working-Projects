34e292c24be51056921df32581c36a30824de4d4
John R. Estes <John.R.Estes@icloud.com>
2023-03-21 07:36:21 -0500

    weather
    restructure API call

--- Practice_JS/26-Weather_App/API_CALL.js
+++ Practice_JS/26-Weather_App/API_CALL.js
@@ -1,9 +1,16 @@
-const URL = 'https://api.open-meteo.com/v1/forecast?latitude=20.36&longitude=-87.59&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation,weathercode,surface_pressure,windspeed_10m,temperature_80m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_probability_max&current_weather=true&timezone=America%2FJamaica';
+const URL = 'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation,weathercode,surface_pressure,windspeed_10m,temperature_80m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_probability_max&current_weather=true&';
 let json;
 
 export async function pullWeather(){
+    const url = (
+        URL + new URLSearchParams({ 
+            latitude: 20.36, 
+            longitude: -87.59,
+            timezone: 'America/Jamaica'
+        })
+      );
     try {
-        const response = await fetch(URL);
+        const response = await fetch(url);
         json = await response.json();
       } catch (error) {
         if (error instanceof SyntaxError) {
