91d1e62cc754a5fbd253855a4b17e2e50776e336
John R. Estes <John.R.Estes@icloud.com>
2023-03-28 08:23:01 -0500

    Calendar
    added function for number of days in Feb, added function to add / sub month

--- Practice_JS/27-Calendar/script.js
+++ Practice_JS/27-Calendar/script.js
@@ -5,29 +5,57 @@ const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 const header = document.querySelector('[data-month]');
 const square = document.querySelectorAll('[data-calendar]');
 const arrow = document.querySelectorAll('[data-arrow]');
+let currentDate, month, year;
 
 fillMonth();
 
-function fillMonth(){
+function fillMonth(today = new Date()){
+    currentDate = today;
     let date = 1;
     let firstDay = false;
-    let today = new Date();
-    let month = today.getMonth();
-    let firstOfMonth = new Date(today.getFullYear(), month, 1, 12, 00, 00, 0);
+    month = today.getMonth();
+    year = today.getFullYear()
+    console.log(year, 'fill month');
+    let firstOfMonth = new Date(year, month, 1, 12, 00, 00, 0);
     let day = firstOfMonth.getDay();
+    let numberOfDays = getDays(year, month);
     header.innerText = MONTHS[month];
     square.forEach((days, index) =>{
         days.dataset.calendar = '';
         if(index === day) firstDay = true;
-        if(firstDay && date <= DAYS_IN_MONTHS[month]){
+        if(firstDay && date <= numberOfDays){
             days.dataset.calendar = date;
             date++;
         }
     })
 }
-
 arrow.forEach(direction =>{
     direction.addEventListener('click', ()=>{
         console.log(direction.dataset.arrow);
+        if(direction.dataset.arrow == 'right'){
+            fillMonth(addMonths(currentDate, 1));
+        } else {
+            fillMonth(subtractMonths(currentDate, 1));
+        }
     })
 })
+function getDays(currentYear, currentMonth){
+    //Feb year check div by 4 and div by 400 but not 100
+    if(currentMonth === 1){
+        if(currentYear % 100 === 0 && currentYear % 400 !== 0) {
+            return 28;}
+        else if(currentYear % 4 !== 0) {
+            return 28;}
+        else {
+            return 29;}
+    }
+    return DAYS_IN_MONTHS[currentMonth];
+}
+function subtractMonths(date, months) {
+    date.setMonth(date.getMonth() - months);
+    return date;
+}
+function addMonths(date, months) {
+    date.setMonth(date.getMonth() + months);
+    return date;
+}
