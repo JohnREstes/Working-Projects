cdcea27301722dbe90d54298c0d785d8440df276
John R. Estes <John.R.Estes@icloud.com>
2023-04-04 15:30:08 -0500

    outputfile
    basic setup

--- Practice_JS/Coding_Challenges/basic_code/script.js
+++ Practice_JS/Coding_Challenges/basic_code/script.js
@@ -1,24 +1,21 @@
 const output = document.querySelector('[data-output]');
 const run = document.querySelector('[data-run]');
 const clear = document.querySelector('[data-clear]');
-
-function addOutput(data){
-    let temp = document.createElement('li');
-    temp.innerText = data;
-    output.appendChild(temp);
-}
+let display = [];
 
 run.onclick = ()=>{
-    addOutput(divide(4,2));
-    addOutput(multiply(4,2))
+
+    
+    addOutput()
 }
 clear.onclick = ()=>{
     output.innerHTML = ""
+    display = [];
 }
-
-function divide(x, y){
-    return x / y;
-}
-function multiply(x, y){
-    return x * y;
+function addOutput(){
+    for(item of display){
+        let temp = document.createElement('li');
+        temp.innerText = item;
+        output.appendChild(temp);
+    }
 }
