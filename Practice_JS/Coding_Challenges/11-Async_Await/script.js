34ae5dcffe7d44012d59443674faf66f3c92f36b
John R. Estes <John.R.Estes@icloud.com>
2023-04-05 15:26:56 -0500

    async

--- Practice_JS/Coding_Challenges/11-Async_Await/script.js
+++ Practice_JS/Coding_Challenges/11-Async_Await/script.js
@@ -5,23 +5,27 @@ let display = [];
 
 run.onclick = ()=>{
 
-    let userList  = getUsers();
-    console.log(userList);
-
-    addOutput()
+    getUsers();
 }
 clear.onclick = ()=>{
     output.innerHTML = ""
     display = [];
 }
-function addOutput(){
-    for(item of display){
-        let temp = document.createElement('li');
-        temp.innerText = item;
-        output.appendChild(temp);
-    }
-}
+
 async function getUsers(){
-    let users = await fetch('https://randomuser.me/api/?results=5');
-    if(users) return users
+    try {
+        let response = await fetch('https://randomuser.me/api/?results=5');
+        let data = await response.json();
+        console.log(data.results);
+        for(item of data.results){
+            let temp = document.createElement('li');
+            let image = document.createElement('img')
+            image.src = item.picture.medium;
+            temp.appendChild(image);
+            output.appendChild(temp);
+        }        
+    } catch (error){
+        console.log(error);
+    }
+
 }
