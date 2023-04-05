4e7f682bd91f2b28a779930358559a3bc9e0de86
John R. Estes <John.R.Estes@icloud.com>
2023-04-05 09:10:57 -0500

    set

--- Practice_JS/Coding_Challenges/8-Set/script.js
+++ Practice_JS/Coding_Challenges/8-Set/script.js
@@ -2,20 +2,10 @@ const output = document.querySelector('[data-output]');
 const run = document.querySelector('[data-run]');
 const clear = document.querySelector('[data-clear]');
 let display = [];
-let allMenus =  [['pizza', 'pasta'], ['pizza', 'calzones'], ['lobster'], ['calzones']]
-
-function uniqueItems(){
-    let combinedMenu = allMenus.flat();
-    let setMenu = new Set();
-    combinedMenu.forEach(item =>{
-        setMenu.add(item);
-    })
-    return setMenu
-}
 
 run.onclick = ()=>{
 
-    display = uniqueItems()
+    
     addOutput()
 }
 clear.onclick = ()=>{
