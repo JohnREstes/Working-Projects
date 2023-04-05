ab1e0dcc7cdb0dea9e2c254d27a68adc73cf00df
John R. Estes <John.R.Estes@icloud.com>
2023-04-05 08:47:20 -0500

    reduce

---
+++ Practice_JS/Coding_Challenges/7-Reduce/script.js
@@ -0,0 +1,28 @@
+const output = document.querySelector('[data-output]');
+const run = document.querySelector('[data-run]');
+const clear = document.querySelector('[data-clear]');
+let display = [];
+const numOfCoffees = [2,3,1,5];
+const priceOfCoffee = 1.25;
+
+run.onclick = ()=>{
+
+    display = total();
+    
+    addOutput()
+}
+clear.onclick = ()=>{
+    output.innerHTML = ""
+    display = [];
+}
+function addOutput(){
+    let temp = document.createElement('li');
+    temp.innerText = display;
+    output.appendChild(temp);
+}
+
+function total(){
+    let sum = numOfCoffees.reduce((prevVal, curVal)=>(prevVal += curVal)
+    );
+    return `The total of coffee is $${(sum * priceOfCoffee)}.`
+}
