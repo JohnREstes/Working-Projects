b0277f431d5d66e3254cbcc1ff59f505c8451d55
John R. Estes <John.R.Estes@icloud.com>
2023-04-05 15:44:42 -0500

    urlify string
    done

--- Practice_JS/Coding_Challenges/12-String/script.js
+++ Practice_JS/Coding_Challenges/12-String/script.js
@@ -2,9 +2,15 @@ const output = document.querySelector('[data-output]');
 const run = document.querySelector('[data-run]');
 const clear = document.querySelector('[data-clear]');
 let display = [];
+let blog1 = "My blog name!"
+let blog2 = "Emma's blog"
+let regPunc = /[\.,-\/#!$%\^&\*;:{}=\-_`~()'@\+\?><\[\]\+]/g;
+let regSpace = /\s/g
 
 run.onclick = ()=>{
 
+    display.push(URLify(blog1));
+    display.push(URLify(blog2));
     
     addOutput()
 }
@@ -18,4 +24,11 @@ function addOutput(){
         temp.innerText = item;
         output.appendChild(temp);
     }
+}
+
+function URLify(value){
+    let clean = value.replace(regPunc, '');
+    let lower = clean.toLowerCase()
+    let dash = lower.replace(regSpace, '-');
+    return dash
 }
