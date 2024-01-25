3c447953254fd28caabb2d3c1ed42b27db84b746
John R. Estes <John.R.Estes@icloud.com>
2024-01-25 15:43:35 -0500

    reordered code

--- Victron/generator.py
+++ Victron/generator.py
@@ -16,58 +16,6 @@ generatorRunning = False
 GPIO.setmode(GPIO.BCM)
 
 
-async def fetch_data(url):
-    try:
-        with requests.Session() as session:
-            response = session.get(url)
-            response.raise_for_status()
-            return response.json()
-    except (requests.exceptions.RequestException, json.JSONDecodeError) as error:
-        print("Error:", error)
-        raise error
-
-
-async def send_status(status):
-    try:
-        with requests.Session() as session:
-            response = session.get(STATUS_URL, params={"message": status})
-            response.raise_for_status()
-            data = response.json()
-            print("Server response:", data)
-    except requests.exceptions.RequestException as error:
-        print("Error sending test GET request:", error)
-
-
-async def main():
-    await start_generator()
-    try:
-        while True:
-            data = await fetch_data(DATA_URL)
-
-            voltage_value = next(
-                (
-                    item["formattedValue"]
-                    for item in data
-                    if item["description"] == "Voltage"
-                ),
-                None,
-            )
-
-            if voltage_value is not None:
-                print("Voltage:", voltage_value)
-            else:
-                print("Voltage information not found in the data.")
-            await send_status(generatorRunning)
-            # Wait for 60 seconds before the next iteration
-            await asyncio.sleep(60)
-
-    except asyncio.CancelledError:
-        # This exception will be raised when the program is stopped
-        pass
-    except Exception as error:
-        print("Error:", error)
-
-
 async def start_generator():
     GPIO.setup(START_PIN, GPIO.OUT)
     if not generatorRunning:
@@ -139,6 +87,58 @@ def open_gas_valve(state):
     except KeyboardInterrupt:
         # Clean up GPIO on script exit
         GPIO.cleanup()
+
+
+async def fetch_data(url):
+    try:
+        with requests.Session() as session:
+            response = session.get(url)
+            response.raise_for_status()
+            return response.json()
+    except (requests.exceptions.RequestException, json.JSONDecodeError) as error:
+        print("Error:", error)
+        raise error
+
+
+async def send_status(status):
+    try:
+        with requests.Session() as session:
+            response = session.get(STATUS_URL, params={"message": status})
+            response.raise_for_status()
+            data = response.json()
+            print("Server response:", data)
+    except requests.exceptions.RequestException as error:
+        print("Error sending test GET request:", error)
+
+
+async def main():
+    await start_generator()
+    try:
+        while True:
+            data = await fetch_data(DATA_URL)
+
+            voltage_value = next(
+                (
+                    item["formattedValue"]
+                    for item in data
+                    if item["description"] == "Voltage"
+                ),
+                None,
+            )
+
+            if voltage_value is not None:
+                print("Voltage:", voltage_value)
+            else:
+                print("Voltage information not found in the data.")
+            await send_status(generatorRunning)
+            # Wait for 60 seconds before the next iteration
+            await asyncio.sleep(60)
+
+    except asyncio.CancelledError:
+        # This exception will be raised when the program is stopped
+        pass
+    except Exception as error:
+        print("Error:", error)
 
 
 if __name__ == "__main__":
