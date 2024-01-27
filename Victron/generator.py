9ca9cd65373b1fc67edb89a805ecb918cc59b5b6
John R. Estes <John.R.Estes@icloud.com>
2024-01-27 06:37:07 -0500

    added both to message

--- Victron/generator.py
+++ Victron/generator.py
@@ -128,7 +128,10 @@ async def send_status(url):
     global generatorRunning, requestToRun
 
     try:
-        status_data = {"generatorRunning": generatorRunning}
+        status_data = {
+            "generatorRunning": generatorRunning,
+            "requestToRun": requestToRun,
+        }
 
         with requests.Session() as session:
             response = session.get(url, params={"message": json.dumps(status_data)})
