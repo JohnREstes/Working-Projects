9353257cd2c8865bf0501d4a0d1d63bda2491c5d
John Estes <john.r.estes@icloud.com>
2024-06-18 16:22:47 -0500

    Update to code

--- Growatt/growatt.py
+++
@@ -1,6 +0,0 @@
-import growattServer
-
-api = growattServer.GrowattApi()
-login_response = api.login("johnnie321", "133Utica")
-#Get a list of growatt plants.
-print(api.plant_list(login_response['user']['id']))
